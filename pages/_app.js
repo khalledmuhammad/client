f9a0b422780d160f16c0d01e9bd6315ff1496d5d
khaled muhammed <Khaled.Saad@appittuneit.co.uk>
2022-10-23 23:27:34 +0200

    client

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import TopNav from "../components/TopNav";
+import { ThemeProvider } from "../context/theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <TopNav />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/admin/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import AdminLayout from '../../components/layout/AdminLayout'
+
+const Admin = () => {
+  return (
+    <AdminLayout>
+    <h1>This is admin page props...</h1>
+    <p>More content...</p>
+  </AdminLayout>
+  )
+}
+
+export default Admin
