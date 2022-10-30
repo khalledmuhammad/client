3b16e357cb17e6dc22fd5a0bd00862b7cbefa0b1
khaled muhammed <Khaled.Saad@appittuneit.co.uk>
2022-10-30 19:06:52 +0200

    addding more changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import TopNav from "../components/TopNav";
 import { ThemeProvider } from "../context/theme";
 import { AuthProvider } from "../context/auth";
+import { PostProvider } from "../context/post";
+
 import "../public/css/styles.css";
 import  {Toaster} from "react-hot-toast";
 
@@ -10,9 +12,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
       <AuthProvider>
+        <PostProvider>
       <Toaster />
       <TopNav />
       <Component {...pageProps} />
+      </PostProvider>
       </AuthProvider>
     </ThemeProvider>
   );
